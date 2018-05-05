21a3ae7888ab9b5427215d01fd694dc252d985fb
Dag Evensberget <dag.evensberget@gmail.com>
2018-05-05 18:26:21 +1000

    Rewrote plot function

--- magnetogram/plot_magnetogram.py
+++ magnetogram/plot_magnetogram.py
@@ -1,70 +1,182 @@
-import matplotlib as mpl
-import matplotlib.pyplot as plt
-from matplotlib import cm, colors
-from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
-from scipy.special import lpmv
-from scipy.special import factorial
+import scipy as sp
+
+import logging
+log = logging.getLogger(__name__)
 
 
 # Evaluate spherical harmonics for on a polar, azimuthal grid)
-def evaluate_spherical(data, polar, azimuth, r=None, r0=1, rss=3):
-    if r is None:
-        r = r0
+def evaluate_real_magnetogram_stanford_pfss_reference(degreee_l, ordere_m, cosine_coefficients_g, sine_coefficients_h,
+                                                      points_polar, points_azimuth, radius=None, r0=1, rss=3):
+    if radius is None:
+        radius = r0
 
-    B_radial = np.zeros(azimuth.shape)
-    B_polar = np.zeros(azimuth.shape)
-    B_azimuthal = np.zeros(azimuth.shape)
+    field_radial = np.zeros_like(points_azimuth)
+    field_polar = np.zeros_like(field_radial)
+    field_azimuthal = np.zeros_like(field_radial)
 
-    for row_id in range(data.shape[0]):
+    for row_id in range(len(degreee_l)):
 
-        l = data.item(row_id, 0);
-        m = data.item(row_id, 1);
-        g = data.item(row_id, 2);
-        h = data.item(row_id, 3);
+        degree_l = degreee_l[row_id]
+        order_m = ordere_m[row_id]
+        g_lm = cosine_coefficients_g[row_id]
+        h_lm = sine_coefficients_h[row_id]
 
-        Pml = lpmv(m, l, np.cos(polar));
+        p_lm = sp.special.lpmv(order_m, degree_l, np.cos(points_polar))
 
         if True:  # Apply correction
             # https://en.wikipedia.org/wiki/Spherical_harmonics#Condon%E2%80%93Shortley_phase
             # https://en.wikipedia.org/wiki/Spherical_harmonics#Conventions
-            d0 = 0 + (m == 0)
-            Pml *= (-1) ** m * np.sqrt(factorial(l - m) / factorial(l + m)) * np.sqrt(2 - d0)
+            d0 = 0 + (order_m == 0)
+            p_lm *= (-1) ** order_m * np.sqrt(sp.special.factorial(degree_l - order_m) / sp.special.factorial(degree_l + order_m)) * np.sqrt(2 - d0)
 
         # Estimate DPml
-        DPml = (Pml - np.roll(Pml, 1)) / (np.cos(polar) - np.roll(np.cos(polar), 1)) * np.sin(polar)
+        DPml = (p_lm - np.roll(p_lm, 1)) / (np.cos(points_polar) - np.roll(np.cos(points_polar), 1)) * np.sin(points_polar)
 
-        fixed = (r0 / r) ** (l + 2) / (l + 1 + l * (r0 / rss) ** (2 * l + 1))
+        fixed = (r0 / radius) ** (degree_l + 2) / (degree_l + 1 + degree_l * (r0 / rss) ** (2 * degree_l + 1))
 
-        B_radial += Pml * (g * np.cos(m * azimuth) + h * np.sin(m * azimuth)) * (
-                l + 1 + l * (r / rss) ** (2 * l + 1)) * fixed;
-        B_polar -= DPml * (g * np.cos(m * azimuth) + h * np.sin(m * azimuth)) * (1 - (r / rss) ** (2 * l + 1)) * fixed;
-        B_azimuthal += Pml * (g * np.sin(m * azimuth) - h * np.cos(m * azimuth)) * (
-                1 - (r / rss) ** (2 * l + 1)) * fixed;
+        field_radial += p_lm * (g_lm * np.cos(order_m * points_azimuth) + h_lm * np.sin(order_m * points_azimuth)) * (
+                degree_l + 1 + degree_l * (radius / rss) ** (2 * degree_l + 1)) * fixed
+        field_polar -= DPml * (g_lm * np.cos(order_m * points_azimuth) + h_lm * np.sin(order_m * points_azimuth)) * (1 - (radius / rss) ** (2 * degree_l + 1)) * fixed
+        field_azimuthal += p_lm * (g_lm * np.sin(order_m * points_azimuth) - h_lm * np.cos(order_m * points_azimuth)) * (
+                1 - (radius / rss) ** (2 * degree_l + 1)) * fixed
 
-    return B_radial, B_polar, B_azimuthal
+    return field_radial, field_polar, field_azimuthal
+
+def theta_lm(deg_l, ord_m, points_polar):
+    """
+    Calculate $\Theta_{\ell m}(\theta)$
+    :param deg_l: Degree $\ell$ of coefficient
+    :param ord_m: Order $m$ of coefficient
+    :param points_polar: 2d array of polar coordinate values
+    :return: 2d array of $\Theta_{\ell m}(\theta)$ values
+    """
+    #  Associated Legendre polynomial $P_{\ell}^{m}(\cos\theta)$
+    plm = sp.special.lpmv(ord_m, deg_l, np.cos(points_polar))
+    # Correction (scaling)
+    d0 = 0 + (ord_m == 0)
+    clm = (-1) ** ord_m * np.sqrt(sp.special.factorial(deg_l - ord_m)
+                                  / sp.special.factorial(deg_l + ord_m)) * np.sqrt(2 - d0)
+
+    clm_plm = clm * plm
+    # Estimate derivative of Legendre polynomial (TODO different approach??)
+    clm_d_plm = (clm_plm - np.roll(clm_plm, 1)) / (np.cos(points_polar) - np.roll(np.cos(points_polar), 1)) * np.sin(points_polar)
+
+    return clm_plm, clm_d_plm
 
 
-def pretty_plot(polar, azimuth, z, ax, crange=(), cmap='RdBu_r'):
-    if len(crange) == 1:
-        cmin = -crange[0]
-        cmax = crange[0]
-    elif len(crange) == 2:
-        cmin = crange[0]
-        cmax = crange[1]
+def r_l(deg_l, r, r0, rss):
+    """
+    Calculate $R_\ell(r;r_\star, r_\text{ss})$
+    :param deg_l: Degree $\ell$ of coefficient
+    :param r: Evaluation radius
+    :param r0: Stellar radius
+    :param rss: Source surface radius
+    :return: value of $R_\ell(r;r_\star, r_\text{ss})$
+    """
+    #
+    # Calculate r_l
+    #
+    # Calculate $\frac{r_\star^{\ell+2}}{r^{\ell+1}}$
+    a = r * (r0 / r) ** (deg_l + 2)
+    # Calculate $\frac{r^{2\ell+1}}{r_\text{ss}^{2\ell+1}}$
+    b = 1 - (r/rss)**(2*deg_l + 1)
+    # Calculate $\ell + 1 + \ell \left(\frac{r_\star}{r_\text{ss}}\right)^{2\ell+1}$
+    e = deg_l + 1 + deg_l * (r0/rss)**(2*deg_l + 1)
+    val = a * b / e
+
+    #
+    # Calculate r_l'
+    #
+    c = (r0/r)**(deg_l + 2)
+    d = deg_l + 1 + deg_l * (r/rss)**(2*deg_l + 1)
+    deriv = -c * d / e
+
+    return val, deriv
+
+
+def phi_lm(ord_m, g_lm, h_lm, phi):
+    """
+    Calculate $Phi_{\ell m}(\phi)$
+    :param ord_m: Order $m$ of coefficient
+    :param g_lm: Cosine-like real $g_{\ell m}$ coefficient
+    :param h_lm: Sine-like real  $h_{\ell m}$  coefficient
+    :param phi: 2d array of azimuth coordinate values
+    :return: 2d array of $Phi_{\ell m}(\phi)$ values
+    """
+    val = g_lm * np.cos(ord_m * phi) + h_lm * np.sin(ord_m * phi)
+    deriv = ord_m * (h_lm * np.cos(ord_m * phi) - g_lm * np.sin(ord_m * phi))
+
+    return val, deriv
+
+
+def evaluate_real_magnetogram_stanford_pfss(
+        degree_l, order_m, cosine_coefficients_g, sine_coefficients_h,
+        points_polar, points_azimuth,
+        radius=None, radius_star=1, radius_source_surface=3):
+    """
+    Evaluate the radial, polar and azimuthal field components of a magnetogram represented
+    as a set of real spherical harmonics on Stanford PFSS form.
+    :param degree_l: Degree $\ell$ of coefficients
+    :param order_m:  Order $m$ of coefficients
+    :param cosine_coefficients_g: Cosine-like real $g_{\ell m}$ coefficients
+    :param sine_coefficients_h: Sine-like real  $h_{\ell m}$  coefficients
+    :param points_polar: 2d array of polar coordinate values
+    :param points_azimuth: 2d array of azimuth coordinate values
+    :param radius: Evaluation radius
+    :param radius_star: Stellar radius
+    :param radius_source_surface: Source surface radius
+    :return: 
+    """
+    assert(points_polar.shape == points_azimuth.shape)
+
+    if radius is None:
+        radius = radius_star
+
+    # Initialize field variables
+    field_radial = np.zeros_like(points_polar)
+    field_polar = np.zeros_like(field_radial)
+    field_azimuthal = np.zeros_like(field_radial)
+
+    # Loop over magnetogram coefficient lines
+    for (deg_l, ord_m, g_lm, h_lm) in zip(degree_l, order_m, cosine_coefficients_g, sine_coefficients_h):
+
+        print(deg_l)
+
+        _r_l = r_l(deg_l, radius, radius_star, radius_source_surface)
+        _theta_lm = theta_lm(deg_l, ord_m, points_polar)
+        _phi_lm = phi_lm(ord_m, g_lm, h_lm, points_azimuth)
+
+        _unscaled_radial  = _r_l[1] * _theta_lm[0] * _phi_lm[0]
+        _unscaled_polar   = _r_l[0] * _theta_lm[1] * _phi_lm[0]
+        _unscaled_azimuth = _r_l[0] * _theta_lm[0] * _phi_lm[1]
+
+        field_radial    -= _unscaled_radial
+        field_polar     -= _unscaled_polar/radius
+        field_azimuthal -= _unscaled_azimuth/(np.sin(points_azimuth * radius))
+
+    return field_radial, field_polar, field_azimuthal
+
+
+def pretty_plot(polar, azimuth, z, ax, color_range=(), color_map='RdBu_r'):
+
+    if len(color_range) == 1:
+        color_min = -color_range[0]
+        color_max = color_range[0]
+    elif len(color_range) == 2:
+        color_min = color_range[0]
+        color_max = color_range[1]
     else:
-        cmax = np.absolute(z[~np.isnan(z)]).max()
-        cmin = np.absolute(z[~np.isnan(z)]).min()
-        # cmax = np.ceil(cmax)
-        # cmin = np.floor(cmin)
+        color_max = np.absolute(z[~np.isnan(z)]).max()
+        color_min = np.absolute(z[~np.isnan(z)]).min()
 
-    # print ('Color map %s, range [%f, %f].' % (cmap, cmin, cmax))
+    # print ('Color map %s, range [%f, %f].' % (color_map, color_min, color_max))
 
     # Print zero contour
     ax.contour(180 / np.pi * azimuth.T, 180 / np.pi * polar.T, z.T, levels=[0], colors=('g',), linewidths=.25)
 
-    q = ax.contourf(180 / np.pi * azimuth.T, 180 / np.pi * polar.T, z.T, 128, cmap=cmap,
-                    vmin=cmin, vmax=cmax)
+    q = ax.contourf(180 / np.pi * azimuth.T, 180 / np.pi * polar.T, z.T, 128, cmap=color_map,
+                    vmin=color_min, vmax=color_max)
 
     ax.set_xticks(180 / np.pi * np.linspace(azimuth[0, 0], azimuth[-1, -1], 9))
     ax.set_label('Azimuth angle $\phi$')

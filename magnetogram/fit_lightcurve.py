f6eed5d79593f3f071457a1ba3dc9e2af52d883d
Dag Evensberget <dag.evensberget@gmail.com>
2018-06-07 19:31:58 +1000

    Both emission and absorbtion working. Fit center as well as the profile parameters.

--- magnetogram/fit_lightcurve.py
+++ magnetogram/fit_lightcurve.py
@@ -4,36 +4,45 @@ import scipy.special
 from scipy import integrate
 
 
-class LightCurve(object):
+
+class EmissionCurve(object):
     """
-    A light curve
+    A light emission curve centered at center
     """
-    def __init__(self, center):
+    def __init__(self, center, shape):
         self._center = center
+        self._shape = shape
 
     def __call__(self, x):
         x_centered = x - self.center
-        return self.evaluate(x_centered)
+        return self._shape(x_centered)
 
+    @property
+    def center(self):
+        """
+        Return center of light curve.
+        :return: Center of light curve.
+        """
+        return self._center
+
+    @property
+    def equivalent_width(self):
+        print(self._shape)
+        return self._shape.equivalent_width
+
+
+class Curve(object):
+    """
+    A curve suitable for use as  a light curve
+    """
+    pass
     @property
     def height(self):
         """
-        Return height of light curve
+        Return height of curve
         :return: Highest value of light curve, assumed to be at the centre of the curve.
         """
-        return self(self.center)
-
-    @property
-    def fwhm(self, x0=1.5):
-        """
-        Return the full width half maximum of the light curve, i.e. the distance along the x axis from the light
-        curve's maximum to the point where it's value is half of the maximum value.
-        :param x0: initial guess
-        :return: full width half maximum of light curve
-        """
-        height = self.height
-        root = optimize.newton(lambda x: self(x) - height / 2, x0)
-        return root * 2
+        return self(0)
 
     @property
     def equivalent_width(self):
@@ -54,28 +63,26 @@ class LightCurve(object):
         area, _ = scipy.integrate.quad(self, -np.inf, np.inf)
         return area
 
-    @property
-    def center(self):
-        """
-        Return center of light curve.
-        :return: Center of light curve.
-        """
-        return self._center
+    # @property
+    # def fwhm(self, x0=1.5):
+    #     """
+    #     Return the full width half maximum of the light curve, i.e. the distance along the x axis from the light
+    #     curve's maximum to the point where it's value is half of the maximum value.
+    #     :param x0: initial guess
+    #     :return: full width half maximum of light curve
+    #     """
+    #     height = self.height
+    #     root = optimize.newton(lambda x: self(x) - height / 2, x0)
+    #     return root * 2
 
-    @center.setter
-    def center(self, val):
-        self._center = val
-
-
-class Gaussian(LightCurve):
+class Gaussian(Curve):
     """
     Gaussian profile
     """
-    def __init__(self, center, sigma):
-        super().__init__(center)
+    def __init__(self, sigma):
         self.sigma = sigma
 
-    def evaluate(self, x):
+    def __call__(self, x):
         num = np.exp(-x ** 2 / (2 * self.sigma ** 2))
         den = self.sigma * np.sqrt(2 * np.pi)
         return num / den
@@ -88,15 +95,14 @@ class Gaussian(LightCurve):
         return 2 * np.sqrt(2 * np.log(2)) * self.sigma
 
 
-class Lorentzian(LightCurve):
+class Lorentzian(Curve):
     """
     Lorentzian profile
     """
-    def __init__(self, center, gamma):
-        super().__init__(center)
+    def __init__(self, gamma):
         self.gamma = gamma
 
-    def evaluate(self, x):
+    def __call__(self, x):
         den = np.pi * (x ** 2 + self.gamma ** 2)
         return self.gamma / den
 
@@ -108,7 +114,7 @@ class Lorentzian(LightCurve):
         return 2 * self.gamma
 
 
-class Voigt(LightCurve):
+class Voigt(Curve):
     """
     A Voigt type light curve
     """
@@ -120,10 +126,9 @@ class PseudoVoigt(Voigt):
     Pseudo-Voigt profile from
     https://en.wikipedia.org/wiki/Voigt_profile#Pseudo-Voigt_approximation
     """
-    def __init__(self, center, sigma, gamma):
-        super().__init__(center)
-        self.gaussian = Gaussian(center, sigma)
-        self.lorentzian = Lorentzian(center, gamma)
+    def __init__(self, sigma, gamma):
+        self.gaussian = Gaussian(sigma)
+        self.lorentzian = Lorentzian(gamma)
         self.eta = self.calculate_eta()
 
     def calculate_eta(self):
@@ -141,7 +146,7 @@ class PseudoVoigt(Voigt):
         fLf = f_L / f
         return 1.36603 * fLf - 0.47719 * fLf ** 2 + 0.11116 * fLf ** 3
 
-    def evaluate(self, x):
+    def __call__(self, x):
         values = 0
         values += self.eta * self.lorentzian(x)
         values += (1 - self.eta) * self.gaussian(x)
@@ -155,12 +160,11 @@ class FaddeevaVoigt(Voigt):
     """
     Voigt profile using the Faddeeva function
     """
-    def __init__(self, center, sigma, gamma):
-        super().__init__(center)
+    def __init__(self, sigma, gamma):
         self.sigma = sigma
         self.gamma = gamma
 
-    def evaluate(self, x):
+    def __call__(self, x):
         z = (x + 1j * self.gamma) / (self.sigma * np.sqrt(2))
         wofz_real = np.real(scipy.special.wofz(z))
         return wofz_real / (self.sigma * np.sqrt(2 * np.pi))
@@ -173,12 +177,11 @@ class ConvolutionVoigt(Voigt):
     """
     Voigt profile with explicit convolution
     """
-    def __init__(self, center, sigma, gamma):
-        super().__init__(center)
-        self.gaussian = Gaussian(center, sigma)
-        self.lorentzian = Lorentzian(center, gamma)
+    def __init__(self, sigma, gamma):
+        self.gaussian = Gaussian(sigma)
+        self.lorentzian = Lorentzian(gamma)
 
-    def evaluate(self, x):
+    def __call__(self, x):
         values = np.convolve(
             self.gaussian(x),
             self.lorentzian(x),
@@ -227,58 +230,70 @@ def demo_plot(light_curve, ax=plt.gca(), xrange=(-10,10)):
         print("Equivalent width not supported by %s: %s" % (light_curve, e))
 
 
-def fit(x, y, LightCurve=Gaussian, guess=None):
+def fit(x, y, profile, absorbtion=True, guess=None):
 
     def logme(names, values, errors=None):
         if errors is None:
-            errors = np.zeros_like(values)
+            errors = [0.0]*len(values)
 
         return_value = ""
         for key, value, error in zip(names, values, errors):
+            # print(key)
+            # print(value)
+            # print(error)
             return_value += ' %s=%f+-%f,' % (key, value, error)
 
         return_value = return_value[:-1] + '.'
         return return_value
 
     if guess is None:
+        param_names = ['center']
+        param_guesses = [np.mean(x)]
         import inspect
-        sig = inspect.signature(LightCurve)
-        params = sig.parameters
-        names = params.keys()
-        initial_guesses = np.ones(len(params.keys()))
-
-    # Guess for center in middle of range
-    initial_guesses[0] = np.mean(x)
-
-    print('Initial guesses:' + logme(names, initial_guesses))
-
-    def fitting_function(x, *args):
-        light_curve = LightCurve(*args)
-        return light_curve(x)
-
-    popt, pcov = scipy.optimize.curve_fit(fitting_function, x, y, p0=initial_guesses)
-
-    fitted_curve = LightCurve(*popt)
-    print('Final fit of %s: %s' % (fitted_curve, logme(names, popt, np.sqrt(np.diag(pcov)))))
-    print('%s height=%f, equivalent width=%f' % (fitted_curve, fitted_curve.height, fitted_curve.equivalent_width))
-
-    return fitted_curve
+        curve_params = inspect.signature(profile).parameters
+        param_names.extend(curve_params.keys())
+        param_guesses.extend([1] * len(curve_params.keys()))
 
 
-def fit_test(true_light_curve, FitLightCurve, xrange=None):
+    print('Initial guesses:' + logme(param_names, param_guesses))
+
+    def fitting_function(x, center, *args, absorbtion=absorbtion):
+        fitted_values = profile(*args)(x - center)
+        if absorbtion:
+            fitted_values = 1 - fitted_values
+
+        return fitted_values
+
+
+    popt, pcov = scipy.optimize.curve_fit(fitting_function, x, y, p0=param_guesses)
+
+    center = popt[0]
+    fitted_values = profile(*popt[1:])
+    print('Final fit of %s: %s' % (fitted_values, logme(param_names, popt, np.sqrt(np.diag(pcov)))))
+    print('%s height=%f, equivalent width=%f' % (fitted_values, fitted_values.height, fitted_values.equivalent_width))
+
+    return center, fitted_values
+
+
+def fit_test(true_profile, center=0, absorbtion=False, profile=Gaussian, xrange=None):
     if xrange is None:
         xrange = np.array([-1.0, 1.0])
-        xrange *= 3.0 * true_light_curve.equivalent_width
-        xrange += true_light_curve.center
+        xrange *= 3.0 * true_profile.equivalent_width
+        xrange += center
     x = np.linspace(*xrange, 200)
-    true_data = true_light_curve(x)
+    true_data = true_profile(x - center)
+    if absorbtion:
+        true_data = 1 - true_data
     noise = np.random.normal(size=x.shape, scale=0.1)
 
     true_line, = plt.plot(x, true_data, label='True data')
     plt.plot(x, true_data + noise, 'x', color=true_line.get_color(), label='Noisy data')
 
-    light_curve = fit(x, true_data + noise, FitLightCurve)
-    plt.plot(x, light_curve(x), '--', label=light_curve)
+    fitted_center, light_curve = fit(x, true_data + noise, profile, absorbtion)
+    fitted_values = light_curve(x - fitted_center)
+    if absorbtion:
+        fitted_values = 1 - fitted_values
+    plt.plot(x, fitted_values, '--', label=light_curve)
 
     plt.grid(True)
     plt.legend()
@@ -287,23 +302,30 @@ def fit_test(true_light_curve, FitLightCurve, xrange=None):
 
 def basic_test():
 
-    demo_plot(Gaussian(0, 1))
-    demo_plot(Lorentzian(0, 1))
-    demo_plot(PseudoVoigt(0, 1, 1))
-    demo_plot(FaddeevaVoigt(0, 1, 1))
-    demo_plot(ConvolutionVoigt(0, 1, 1))
+    demo_plot(Gaussian(1))
+    demo_plot(Lorentzian(1))
+    demo_plot(PseudoVoigt(1, 1))
+    demo_plot(FaddeevaVoigt(1, 1))
+    demo_plot(ConvolutionVoigt(1, 1))
     plt.grid(True)
     plt.legend()
     plt.show()
 
 
 if __name__ == "__main__":
-    basic_test()
-    fit_test(Gaussian(0, 1), Gaussian)
-    fit_test(Gaussian(10, 1), Gaussian)
-    fit_test(Gaussian(0, 1), Lorentzian)
-    fit_test(Gaussian(10, 1), Lorentzian)
-    fit_test(Gaussian(0, 1), FaddeevaVoigt)
-    fit_test(Gaussian(10, 1), FaddeevaVoigt)
-    fit_test(Gaussian(0, 1), PseudoVoigt)
+    # basic_test()
+    fit_test(Gaussian(1), center=1, absorbtion=False, profile=Gaussian)
+    fit_test(Gaussian(1), center=1, absorbtion=False, profile=Gaussian)
+    fit_test(Gaussian(1), center=1, absorbtion=False, profile=Lorentzian)
+    fit_test(Gaussian(1), center=1, absorbtion=False, profile=Lorentzian)
+    fit_test(Gaussian(1), center=1, absorbtion=False, profile=FaddeevaVoigt)
+    fit_test(Gaussian(1), center=1, absorbtion=False, profile=FaddeevaVoigt)
+    fit_test(Gaussian(1), center=1, absorbtion=False, profile=PseudoVoigt)
 
+    fit_test(Gaussian(1), center=10, absorbtion=True, profile=Gaussian)
+    fit_test(Gaussian(1), center=10, absorbtion=True, profile=Gaussian)
+    fit_test(Gaussian(1), center=10, absorbtion=True, profile=Lorentzian)
+    fit_test(Gaussian(1), center=10, absorbtion=True, profile=Lorentzian)
+    fit_test(Gaussian(1), center=-10, absorbtion=True, profile=FaddeevaVoigt)
+    fit_test(Gaussian(1), center=0, absorbtion=True, profile=FaddeevaVoigt)
+    fit_test(Gaussian(1), center=13, absorbtion=True, profile=PseudoVoigt)

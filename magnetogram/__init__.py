317a209d072ec9bf04cdfc2f707622bce4c7fbcb
Dag Evensberget <dag.evensberget@gmail.com>
2018-09-17 19:06:35 +1000

    Should __init__ files be empty?

--- magnetogram/__init__.py
+++ magnetogram/__init__.py
@@ -1,16 +1,18 @@
-"""
-Magnetogram conversion tools
+# TODO This file should be empty, or have content???
 
-    :copyright:
-    :license:
-"""
-
-from .convert_magnetogram import convert_magnetogram_file
-
-# For use with versioneer
-'''
-from ._version import get_versions
-__version__ = get_versions()['version']
-del get_versions
-'''
-__version__ = "0.1.0"
+# """
+# Magnetogram conversion tools
+#
+#     :copyright:
+#     :license:
+# """
+#
+# from .convert_magnetogram import convert_magnetogram_file
+#
+# # For use with versioneer
+# '''
+# from ._version import get_versions
+# __version__ = get_versions()['version']
+# del get_versions
+# '''
+# __version__ = "0.1.0"

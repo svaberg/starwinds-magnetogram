f2ed00899a7a75445cf4896a5bfc5ee1ef0a2c18
Dag Evensberget <dag.evensberget@gmail.com>
2018-11-16 14:24:22 +1000

    Only plot zero line if there is one

--- magnetogram/plot_magnetogram.py
+++ magnetogram/plot_magnetogram.py
@@ -183,7 +183,8 @@ def pretty_plot(polar, azimuth, z, ax, color_range=(), color_map='RdBu_r'):
     log.info('Color map %s, range [%f, %f].' % (color_map, color_min, color_max))
 
     # Print zero contour
-    ax.contour(180 / np.pi * azimuth.T, 180 / np.pi * polar.T, z.T, levels=[0], colors=('g',), linewidths=.25)
+    if np.min(z) < 0 < np.max(z):
+        ax.contour(180 / np.pi * azimuth.T, 180 / np.pi * polar.T, z.T, levels=[0], colors=('g',), linewidths=.25)
 
     q = ax.contourf(180 / np.pi * azimuth.T, 180 / np.pi * polar.T, z.T, 128, cmap=color_map,
                     vmin=color_min, vmax=color_max)

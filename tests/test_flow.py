a9c91750f85385c446ffa8f727fe75e71caef72f
Dag Evensberget <dag.evensberget@gmail.com>
2019-12-19 17:49:36 +1000

    Vectorise lic

--- tests/test_flow.py
+++ tests/test_flow.py
@@ -1,11 +1,26 @@
 import logging
 log = logging.getLogger(__name__)
 import numpy as np
+import matplotlib
 import pytest
+
+from contextlib import contextmanager
+from time import time
+
 
 from tests import context  # Test context
 
 
+@contextmanager
+def timing(description: str) -> None:
+    start = time()
+    yield
+    elapsed_time = time() - start
+
+    log.info(f"{description}: {elapsed_time}")
+
+
+# Think this is a version of Bresenham's algorithm.
 def lic_flow(vectors, len_pix=10):
     vectors = np.asarray(vectors)
     m, n, two = vectors.shape
@@ -75,134 +90,160 @@ def lic_flow(vectors, len_pix=10):
     return result
 
 
-def numpy_lic_flow(vectors, len_pix=10):
+def lic_flow_numpy(vectors, len_pix=10):
+    """
+
+    """
     vectors = np.asarray(vectors)
     m, n, two = vectors.shape
     if two != 2:
-        raise ValueError
+        raise ValueError("Last dimension must be 2")
 
-    yxids = np.zeros((2 * len_pix + 1, m, n, 2), dtype=np.int32)  # FIXME: int16?
+    yxids = np.zeros((2 * len_pix + 1, m, n, 2), dtype=int)
     center = len_pix
     yxids[center, :, :, 0] = np.arange(m)[:, np.newaxis]
     yxids[center, :, :, 1] = np.arange(n)[np.newaxis, :]
 
-    fxs = 0.5 * np.ones_like(yxids[:, :, :, 0])
-    fys = 0.5 * np.ones_like(yxids[:, :, :, 1])
-    txs = np.zeros_like(yxids[:, :, :, 0])
-    tys = np.zeros_like(yxids[:, :, :, 1])
+    fxys = 0.5 * np.ones_like(yxids, dtype=float)
+    fxs = fxys[..., 0]
+    fys = fxys[..., 1]
+
+    for k in range(center + 1, center + 1 + len_pix):
+
+        yids = yxids[k - 1, :, :, 0]
+        xids = yxids[k - 1, :, :, 1]
+
+        _t = vectors[yids, xids, :]
+        txys = ((_t >= 0) - fxys[k - 1]) / _t
 
 
-    for i in range(m):
-        for j in range(n):
+        #
+        #
+        #
+        yxids[k] = yxids[k-1]
 
-            # fx = fxs[center, i, j]
-            # fy = fys[center, i, j]
+        # X step indices ix, jx
+        ix, jx = np.where(txys[..., 0] < txys[..., 1])
+        _vx = vectors[yids[ix, jx], xids[ix, jx], :]
 
-            for k in range(center+1, center+1+len_pix):
-                yid = yxids[k-1, i, j, 0]
-                xid = yxids[k-1, i, j, 1]
+        # Y step indices iy, jy
+        iy, jy = np.where(txys[..., 0] >= txys[..., 1])
+        _vy = vectors[yids[iy, jy], xids[iy, jy], :]
 
-                if vectors[yid, xid, 0] >= 0:
-                    txs[k, i, j] = (1 - fxs[k-1, i, j]) / vectors[yid, xid, 0]
-                else:
-                    txs[k, i, j] = -fxs[k-1, i, j] / vectors[yid, xid, 0]
-                if vectors[yid, xid, 1] >= 0:
-                    tys[k, i, j] = (1 - fys[k-1, i, j]) / vectors[yid, xid, 1]
-                else:
-                    tys[k, i, j] = -fys[k-1, i, j] / vectors[yid, xid, 1]
+        # These are not merged as ix and iy do not generally have the same length
+        fxys[k, ix, jx, 0] = _vx[..., 0] <= 0
+        fxys[k, ix, jx, 1] = fxys[k - 1, ix, jx, 1] + _vx[..., 1] * txys[ix, jx, 0]
 
-                # Determine step direction.
-                # THis updates xid, yid
-                if txs[k, i, j] < tys[k, i, j]:
-                    # X step
-                    fys[k, i, j] = fys[k-1, i, j] + vectors[yid, xid, 1] * txs[k, i, j]
-                    if vectors[yid, xid, 0] > 0:
-                        xid += 1
-                        fxs[k, i, j] = 0.
-                    else:
-                        xid -= 1
-                        fxs[k, i, j] = 1.
-                else:
-                    # Y step
-                    fxs[k, i, j] = fxs[k-1, i, j] + vectors[yid, xid, 0] * tys[k, i, j]
-                    if vectors[yid, xid, 1] > 0:
-                        yid += 1
-                        fys[k, i, j] = 0.
-                    else:
-                        yid -= 1
-                        fys[k, i, j] = 1.
+        fxys[k, iy, jy, 0] = fxys[k - 1, iy, jy, 0] + _vy[..., 0] * txys[iy, jy, 1]
+        fxys[k, iy, jy, 1] = _vy[..., 1] <= 0
 
-                # del tx, ty
+        yxids[k, iy, jy, 0] += np.sign(_vy[..., 1]).astype(int)
+        yxids[k, ix, jx, 1] += np.sign(_vx[..., 0]).astype(int)
 
-                # Don't go outside the domain
-                if xid < 0:
-                    xid = 0
-                if yid < 0:
-                    yid = 0
-                if xid >= n:
-                    xid = n - 1
-                if yid >= m:
-                    yid = m - 1
+        # Don't go outside the domain
+        yxids[k, :, :, 0] = np.clip(yxids[k, :, :, 0], 0, m-1)
+        yxids[k, :, :, 1] = np.clip(yxids[k, :, :, 1], 0, n-1)
 
-                yxids[k, i, j, :] = yid, xid
-
-    yxids = yxids[center:, ...]
-    return yxids
+    return yxids[center:, ...]
 
 
+def plot_lic(ax, X, Y, U, V):
+    S = (U ** 2 + V ** 2) ** .5
+    ax.pcolormesh(X, Y, S, cmap='RdBu_r')
+    result = add_lic(ax, X, Y, U, V)
 
-def test_lic(request):
 
-    xo = np.linspace(-1, 1, 231)
-    yo = np.linspace(-1, 1, 231)
-    xc = 0.5 * (xo[:-1] + xo[1:])
-    yc = 0.5 * (yo[:-1] + yo[1:])
+def add_lic(ax, X, Y, U, V, length=None, alpha=.1, img=None, seed=None):
+    """
 
-    Xo, Yo = np.meshgrid(xo, yo)
-    Xc, Yc = np.meshgrid(xc, yc)
+    """
+    if length is None:
+        length = np.max(X.shape)//4
 
-    # print(Xo)
-    U = -Yc
-    V = Xc
-    speed = np.sqrt(U ** 2 + V ** 2)
+    if seed is not None:
+        np.random.seed(seed)
 
+    if img is None:
+        img = np.random.random(size=U.shape)
+
+    if not matplotlib.rcParams['image.composite_image']:
+        log.warning("Please enable image.composite_image")
+
+    with timing("Vectorised"):
+        flow = lic_flow_numpy(np.stack((U, V), axis=-1), length)
+
+    smear = np.average(img[flow[..., 0], flow[..., 1]], axis=0)
+
+    img = ax.imshow(smear,
+                    alpha=alpha,
+                    extent=(np.min(X), np.max(X), np.min(Y), np.max(Y)),
+                    origin='lower',
+                    interpolation='bicubic',
+                    cmap='gray',
+                    zorder=10)
+    return img, flow
+
+
+def test_reference(request):
+
+    x = np.linspace(-2, 2, 72)
+    y = np.linspace(-1, 1, 62)
+
+    X, Y = np.meshgrid(x, y)
+
+    U = -Y
+    V = X
+
+    np.random.seed(0)
     noise = np.random.random(size=U.shape)
 
     C = np.zeros_like(U)
     UV = np.stack((U, V), axis=-1)
 
-    result = numpy_lic_flow(UV, 30)
+    with timing("Initial"):
+        result = lic_flow(UV, 30)
+    with timing("Vectorised"):
+        result1 = lic_flow_numpy(UV, 30)
 
-    for j in range(U.shape[0]):
-        for k in range(U.shape[1]):
-            tx = result[:, j, k, 0]
-            ty = result[:, j, k, 1]
-            C[j, k] = np.average(noise[tx, ty])
+    tx = result[..., 0]
+    ty = result[..., 1]
+    C = np.average(noise[tx, ty], axis=0)
 
-    #         if len(tx) > 0:
-    #             plt.plot(Xc[ty, tx], Yc[ty, tx], '-')
-    #             plt.plot(Xc[ty[0], tx[0]], Yc[ty[0], tx[0]], '-x')
-
-
+    tx1 = result1[..., 0]
+    ty1 = result1[..., 1]
+    C1 = np.average(noise[tx1, ty1], axis=0)
 
     # Save and plot result
     with context.PlotNamer(__file__, request.node.name) as (pn, plt):
 
         fig, ax1 = plt.subplots()
-
         S = (U**2 + V**2)**.5
-        # Varying color along a streamline
-        # ax1.pcolormesh(Xo, Yo, U,)
-        strm = ax1.streamplot(Xc, Yc, U, V, color=S)
+        strm = ax1.streamplot(X, Y, U, V, color=S)
         fig.colorbar(strm.lines)
 
         plt.savefig(pn.get())
 
-        fig, [ax1, ax2] = plt.subplots(1, 2)
-        # ax1.pcolormesh(C, alpha=.1, edgecolor=(1.0, 1.0, 1.0, 0.1), linewidth=0.0015625)
-        ax1.pcolormesh(Xo, Yo, S, cmap='RdBu_r')
-        ax1.set_aspect('equal')
-        ax2.imshow(S, extent=(-1, 1, -1, 1), origin='lower', interpolation='bicubic', cmap='RdBu_r')
-        ax2.imshow(C, alpha=.1, extent=(-1, 1, -1, 1), origin='lower', interpolation='bicubic', cmap='gray')
-        ax2.grid(False)
+        fig, axs = plt.subplots(1, 2)
+        for ax, c in zip (axs, (C, C1)):
+            ax.imshow(S, extent=(-1, 1, -1, 1), origin='lower', interpolation='bicubic', cmap='RdBu_r')
+            ax.imshow(c, alpha=.1, extent=(-1, 1, -1, 1), origin='lower', interpolation='bicubic', cmap='gray')
+            ax.grid(False)
+
+        plt.savefig(pn.get())
+
+    assert np.allclose(result, result1)
+
+
+
+def test_lic(request):
+
+    x = np.linspace(-2, 2, 272)
+    y = np.linspace(-1, 1, 262)
+    X, Y = np.meshgrid(x, y)
+    U = -Y
+    V = X
+
+    with context.PlotNamer(__file__, request.node.name) as (pn, plt):
+        fig, ax = plt.subplots(1, 1)
+        plot_lic(ax, X, Y, U, V)
         plt.savefig(pn.get())

a073efdee47091fee52e9bda14fe831160d9260d
Dag Evensberget <dag.evensberget@gmail.com>
2020-05-22 12:51:46 +1000

    Calculate longitudinal field from zdi magnetogram

--- magnetogram/geometry.py
+++ magnetogram/geometry.py
@@ -1,7 +1,7 @@
 import numpy as np
 import logging
 log = logging.getLogger(__name__)
-
+from functools import cached_property
 import stellarwinds.coordinate_transforms
 
 
@@ -27,9 +27,12 @@ class ZdiGeometry:
         self.polar_corners, self.azimuthal_corners = np.meshgrid(polar_corners, azimuthal_corners)
 
     def corners(self):
+        """Facet corner polar coordinates (polar, azimuth)"""
         return self.polar_corners, self.azimuthal_corners
 
+    # @cached_property
     def centers(self):
+        """Facet center polar coordinates (polar, azimuth)"""
         def make_centers(x):
             return 0.25 * (x[:-1, :-1] + x[:-1, 1:] + x[1:, :-1] + x[1:, 1:])
 
@@ -39,16 +42,45 @@ class ZdiGeometry:
         assert azimuthal_centers.shape == polar_centers.shape
         return polar_centers, azimuthal_centers
 
+    def unit_normals(self):
+        """Facet unit normal vector cartesian coordinates as ndarray"""
+        return np.stack(self.centers_cartesian(), axis=-1)
+
+    # def normals(self):
+    #     """Facet normal vector cartesian coordinates as ndarray. The lengths correspond to the facet area."""
+    #     return self.unit_normals() * self.areas()[..., np.newaxis]
+
     def areas(self):
         """Calculate area as difference between two spherical caps
-        https://en.wikipedia.org/wiki/Spherical_cap"""
+        https://en.wikipedia.org/wiki/Spherical_cap
+        TODO this is inconsistent with the faceted approach used elsewhere in this class."""
 
         height_delta = (np.cos(self.polar_corners[:-1, :-1]) - np.cos(self.polar_corners[:-1, 1:]))
         azimuth_delta = (self.azimuthal_corners[1:, 1:] - self.azimuthal_corners[:-1, 1:])
 
         return height_delta * azimuth_delta
 
+    # def visible_areas(self, projection_direction):
+    #     """Calculate projected (visible) area in the direction of projection_direction.
+    #     Invisible facets have zero area."""
+    #     proj_dir_length = np.sum(projection_direction ** 2) ** .5
+    #     projected_areas = np.sum((projection_direction / proj_dir_length) * self.normals(), axis=-1)
+    #     return np.where(projected_areas > 0, projected_areas, 0)
+
+    def projected_visible_area_fraction(self, projection_direction):
+        """
+        Calculate each facet's projected visible area divided by its total area.
+        Invisible facets have zero projected visible area.
+        :param projection_direction: direction of the projection
+        :return:
+        """
+        proj_dir_length = np.sum(projection_direction ** 2) ** .5
+        proj_area_frac = np.sum((projection_direction / proj_dir_length) * self.unit_normals(), axis=-1)
+        return np.where(proj_area_frac > 0, proj_area_frac, 0)
+
+
     def corners_cartesian(self):
+        """Facet corner cartesian coordinates"""
         x_corners, y_corners, z_corners = \
             stellarwinds.coordinate_transforms.rectangular_coordinates_from_spherical(
                 np.ones(self.polar_corners.shape),
@@ -58,6 +90,7 @@ class ZdiGeometry:
         return x_corners, y_corners, z_corners
 
     def centers_cartesian(self):
+        """Facet center cartesian coordinates"""
         polar_centers, azimuthal_centers = self.centers()
         x_centers, y_centers, z_centers = \
             stellarwinds.coordinate_transforms.rectangular_coordinates_from_spherical(
@@ -68,37 +101,38 @@ class ZdiGeometry:
         return x_centers, y_centers, z_centers
 
 
-def numerical_description(geometry, zdi):
+def numerical_description(zdi_geometry, zdi_magnetogram):
     """
     Describe field by numerically evaluating it at a set of points, then taking sums and
     averages.
-    :param geometry:
-    :param zdi:
+    TODO test that this still works.
+    :param zdi_geometry:
+    :param zdi_magnetogram:
     :return:
     """
 
     def describe(name, values):
         log.info("Describing %s component." %  name)
         abs_max_indices = np.unravel_index(np.argmax(np.abs(values), axis=None), values.shape)
-        abs_max_polar = geometry.centers()[0][abs_max_indices]
-        abs_max_azimuth = geometry.centers()[1][abs_max_indices]
+        abs_max_polar = zdi_geometry.centers()[0][abs_max_indices]
+        abs_max_azimuth = zdi_geometry.centers()[1][abs_max_indices]
         abs_max = values[abs_max_indices]
 
-        mean = np.sum(values * geometry.areas()) / (4 * np.pi)
-        abs_mean = np.sum(np.abs(values) * geometry.areas()) / (4 * np.pi)
+        mean = np.sum(values * zdi_geometry.areas()) / (4 * np.pi)
+        abs_mean = np.sum(np.abs(values) * zdi_geometry.areas()) / (4 * np.pi)
 
         log.info("|B|_max = %4.4g Gauss" % (abs_max))
         log.info("|B|_max at az=%2.2f deg, pl=%3.2f deg" % (np.rad2deg(abs_max_azimuth),
                                                             np.rad2deg(abs_max_polar)))
         log.info("|B|_mean = %4.4g Gauss" % abs_mean)
 
-    _dict = zdi.get_all()
+    _dict = zdi_magnetogram.get_all()
 
-    accumulated_strength_squared = np.zeros_like(geometry.centers()[0])
+    accumulated_strength_squared = np.zeros_like(zdi_geometry.centers()[0])
     for key_0, value_0 in _dict.items():
-        accumulated_component = np.zeros_like(geometry.centers()[0])
+        accumulated_component = np.zeros_like(zdi_geometry.centers()[0])
         for key_1, method_1 in value_0.items():
-            values_1 = method_1(*geometry.centers())
+            values_1 = method_1(*zdi_geometry.centers())
             describe(key_0 + "-" + key_1, values_1)
             accumulated_component += values_1
 
---
+++ tests/test_longitudinal_field.py
@@ -0,0 +1,235 @@
+import matplotlib as mpl
+from matplotlib import cm
+import numpy as np
+import logging
+
+# Test "context"
+import stellarwinds.magnetogram.plot_zdi
+from tests import context  # Test context
+from tests.magnetogram import magnetograms
+from tests.magnetogram import test_flow
+log = logging.getLogger(__name__)
+
+# Local
+import stellarwinds.magnetogram.zdi_magnetogram
+import stellarwinds.magnetogram.geometry
+from stellarwinds.magnetogram import plots
+from stellarwinds import coordinate_transforms
+import pytest
+
+
+def test_normals():
+    zg = stellarwinds.magnetogram.geometry.ZdiGeometry(11)
+    unit_normals = zg.unit_normals()
+    assert np.allclose(np.sum(unit_normals**2, axis=-1), 1)
+
+
+def test_observation_angle(request):
+    observer_direction = np.array([0, 1, 1])
+    obs_dir_length = np.sum(observer_direction**2)**.5
+    zg = stellarwinds.magnetogram.geometry.ZdiGeometry(64)
+    unit_normals = zg.unit_normals()
+
+    # Dot product of observer direction and normal vectors
+    cos_angle = np.sum((observer_direction / obs_dir_length) * unit_normals, axis=-1)
+    assert np.all(cos_angle <= 1)
+    assert np.all(cos_angle >= -1)
+
+    with context.PlotNamer(__file__, request.node.name) as (pn, plt):
+        polar, azimuth = zg.centers()
+        fig, ax = plt.subplots()
+        img1 = ax.pcolormesh(np.rad2deg(azimuth), np.rad2deg(polar), np.rad2deg(np.arccos(cos_angle)),
+                             vmin=0, vmax=180)
+        fig.colorbar(img1, ax=ax)
+        ax.invert_yaxis()
+        ax.set_title("Observation angle. Observer at " + str(observer_direction))
+
+        _, p, a = coordinate_transforms.spherical_coordinates_from_rectangular(*observer_direction)
+        plt.plot(np.rad2deg(a), np.rad2deg(p), 'ko')
+
+        plt.savefig(pn.get())
+
+
+def test_visible_area_fraction(request):
+    observer_direction = np.array([0, 0, 1])
+    zg = stellarwinds.magnetogram.geometry.ZdiGeometry(256)
+
+    visible_fraction = zg.projected_visible_area_fraction(observer_direction)
+
+    assert np.all(visible_fraction >= 0)
+    assert np.all(visible_fraction <= 1)
+
+    with context.PlotNamer(__file__, request.node.name) as (pn, plt):
+        polar, azimuth = zg.centers()
+        fig, ax = plt.subplots()
+        img1 = ax.pcolormesh(np.rad2deg(azimuth), np.rad2deg(polar), visible_fraction, vmin=0, vmax=1)
+        fig.colorbar(img1, ax=ax)
+        ax.invert_yaxis()
+        ax.set_title("Visible fraction. Observer at " + str(observer_direction))
+
+        _, p, a = coordinate_transforms.spherical_coordinates_from_rectangular(*observer_direction)
+        plt.plot(np.rad2deg(a), np.rad2deg(p), 'ko')
+
+        plt.savefig(pn.get())
+
+
+def test_parallel_field(request, magnetogram_name="mengel"):
+    """Longitudinal refers to the direction towards the observer."""
+    observer_direction = np.array([0, 1, 1])
+    zg = stellarwinds.magnetogram.geometry.ZdiGeometry(64)
+
+    lz = stellarwinds.magnetogram.zdi_magnetogram.from_coefficients(magnetograms.get_all(magnetogram_name))
+
+    parallel_field = get_parallel_field(zg, lz, observer_direction)
+
+    # Average field over entire surface (including backside). This does not average out to 0.
+    average_proj_field = np.sum(zg.areas() * parallel_field) / np.sum(zg.areas())
+    log.info(f"Sphere average parallel field {average_proj_field}")
+
+    with context.PlotNamer(__file__, request.node.name) as (pn, plt):
+        polar, azimuth = zg.centers()
+        fig, ax = plt.subplots()
+        img1 = ax.pcolormesh(np.rad2deg(azimuth), np.rad2deg(polar), parallel_field,
+                             vmin=-np.max(np.abs(parallel_field)),
+                             vmax=+np.max(np.abs(parallel_field)),
+                             )
+        fig.colorbar(img1, ax=ax)
+        ax.invert_yaxis()
+        ax.set_title(f"Sphere average parallel field {average_proj_field}.")
+
+        _, p, a = coordinate_transforms.spherical_coordinates_from_rectangular(*observer_direction)
+        ax.plot(np.rad2deg(a), np.rad2deg(p), 'ko')
+
+        plt.savefig(pn.get())
+
+
+def test_longitudinal_field(request, magnetogram_name="mengel"):
+    """Longitudinal refers to the direction towards the observer.
+    This is the same as each facet's parallel field multiplied by its visible area"""
+    observer_direction = np.array([0, 1, 1])
+    zg = stellarwinds.magnetogram.geometry.ZdiGeometry(64)
+
+    lz = stellarwinds.magnetogram.zdi_magnetogram.from_coefficients(magnetograms.get_all(magnetogram_name))
+
+    parallel_field = get_parallel_field(zg, lz, observer_direction)
+    projected_visible_area_fraction = zg.projected_visible_area_fraction(observer_direction)
+    visible_areas = zg.areas() * projected_visible_area_fraction
+
+    longitudinal_field = np.sum(parallel_field * visible_areas) / np.sum(visible_areas)
+    log.info(f"Longitudinal field {longitudinal_field}")
+
+    with context.PlotNamer(__file__, request.node.name) as (pn, plt):
+        polar, azimuth = zg.centers()
+        fig, axs = plt.subplots(2, 1)
+        ax = axs[0]
+        img1 = ax.pcolormesh(np.rad2deg(azimuth), np.rad2deg(polar), parallel_field * projected_visible_area_fraction,
+                             vmin=-np.max(np.abs(parallel_field * projected_visible_area_fraction)),
+                             vmax=+np.max(np.abs(parallel_field * projected_visible_area_fraction)),
+                             )
+        fig.colorbar(img1, ax=ax)
+        ax.invert_yaxis()
+
+        _, p, a = coordinate_transforms.spherical_coordinates_from_rectangular(*observer_direction)
+        ax.plot(np.rad2deg(a), np.rad2deg(p), 'ko')
+
+        ax.set_title("Longitudinal field %f" % longitudinal_field)
+        ax.grid()
+        ax = axs[1]
+        _summands = parallel_field * visible_areas / np.sum(visible_areas)
+        img1 = ax.pcolormesh(np.rad2deg(azimuth), np.rad2deg(polar), _summands,
+                             vmin=-np.max(np.abs(_summands)),
+                             vmax=+np.max(np.abs(_summands)),
+                             )
+        fig.colorbar(img1, ax=ax)
+        ax.invert_yaxis()
+        ax.set_title(f"Area corrected longitudinal field summands. Sum %f" % np.sum(_summands))
+        _, p, a = coordinate_transforms.spherical_coordinates_from_rectangular(*observer_direction)
+        ax.plot(np.rad2deg(a), np.rad2deg(p), 'ko')
+        ax.grid()
+        plt.savefig(pn.get())
+
+
+def test_longitudinal_field_curve(request, magnetogram_name="quadrupole"):
+
+    zg = stellarwinds.magnetogram.geometry.ZdiGeometry(64)
+    lz = stellarwinds.magnetogram.zdi_magnetogram.from_coefficients(magnetograms.get_all(magnetogram_name))
+
+    observer_polar = np.deg2rad(50)
+
+    observer_azimuths = np.deg2rad(np.linspace(0, 360, 20))
+    longitudinal_fields = np.empty_like(observer_azimuths)
+
+    areas = zg.areas()
+
+    for i, observer_azimuth in enumerate(observer_azimuths):
+        observer_direction = np.array(
+            coordinate_transforms.rectangular_coordinates_from_spherical(1, observer_polar, observer_azimuth))
+
+        parallel_field = get_parallel_field(zg, lz, observer_direction)
+        projected_visible_area_fraction = zg.projected_visible_area_fraction(observer_direction)
+        visible_areas = areas * projected_visible_area_fraction
+
+        longitudinal_fields[i] = np.sum(parallel_field * visible_areas) / np.sum(visible_areas)
+
+    with context.PlotNamer(__file__, request.node.name) as (pn, plt):
+        polar, azimuth = zg.centers()
+        fig, axs = plt.subplots(2, 1, sharex=True)
+        ax = axs[0]
+        img1 = ax.pcolormesh(np.rad2deg(azimuth), np.rad2deg(polar), parallel_field * projected_visible_area_fraction,
+                             vmin=-np.max(np.abs(parallel_field * projected_visible_area_fraction)),
+                             vmax=+np.max(np.abs(parallel_field * projected_visible_area_fraction)),
+                             )
+        fig.colorbar(img1, ax=axs)
+        ax.invert_yaxis()
+
+        _, p, a = coordinate_transforms.spherical_coordinates_from_rectangular(*observer_direction)
+        ax.plot(np.rad2deg(a), np.rad2deg(p), 'ko')
+
+        # ax.set_title("Longitudinal field %f" % longitudinal_field)
+        ax.grid()
+        ax = axs[1]
+        ax.plot(np.rad2deg(observer_azimuths), longitudinal_fields)
+        ax.grid(True)
+        plt.savefig(pn.get())
+
+
+def get_parallel_field(zdi_geometry, zdi_magnetogram, direction):
+    """
+    Calculate projected magnetic field on the surface facets of the zdi_geometry.
+    To calculate the longitudinal magnetic field this must be averaged over the projected visible surface of each
+    facet.
+    :param zdi_geometry:
+    :param zdi_magnetogram:
+    :param direction: direction vector of projection
+    :return:
+    """
+    # Field components in spherical coordinates
+    fr = zdi_magnetogram.get_radial_field(*zdi_geometry.centers())
+    fp = zdi_magnetogram.get_polar_field(*zdi_geometry.centers())
+    fa = zdi_magnetogram.get_azimuthal_field(*zdi_geometry.centers())
+
+    px, py, pz = [zdi_geometry.unit_normals()[..., i] for i in range(3)]
+    field_xyz = mmm(fa, fp, fr, px, py, pz)
+
+    return np.sum(field_xyz * direction, axis=-1)
+
+
+def mmm(fa, fp, fr, px, py, pz):
+    pr, pp, pa = coordinate_transforms.spherical_coordinates_from_rectangular(px, py, pz)
+    assert pr.shape == px.shape, "Expected matching shapes"
+    assert pp.shape == px.shape, "Expected matching shapes"
+    assert pa.shape == px.shape, "Expected matching shapes"
+    assert fr.shape == pr.shape, "Expected matching shapes"
+    assert fp.shape == pr.shape, "Expected matching shapes"
+    assert fa.shape == pr.shape, "Expected matching shapes"
+    # To carry out the transformation, flatten the polar coordinate arrays and stack them
+    # calculate the transformation matrix, and apply it to the stack field_rpa.
+    field_rpa = np.stack([c.flatten() for c in (fr, fp, fa)], axis=-1)
+    transformation_matrix = coordinate_transforms.spherical_to_rectangular_transformation_matrix(pp.flatten(),
+                                                                                                 pa.flatten())
+    field_xyz = transformation_matrix @ field_rpa[:, :, np.newaxis]
+    # Get rid of last dimension which is has length 1
+    assert field_xyz.shape[-1] == 1, "Expected last dimension size to be 1"
+    assert field_xyz.shape[-2] == 3, "Expected second last dimension size to be 3"
+    field_xyz = field_xyz[..., 0].reshape(px.shape + (3,))
+    return field_xyz
